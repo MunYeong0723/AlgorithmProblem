90f6e066996dff81cc2ea470a236c6dc23a33df8
양문영 <ansdud0723@hanyang.ac.kr>
2020-03-09 23:15:34 +0900

    baekjoon problem #3101 -> wrong answer

--- 3101.kt
+++ 3101.kt
@@ -6,58 +6,88 @@ fun main() {
     val K = sc.nextInt()
     sc.nextLine()
 
-    // make board
-    //var jump = Array(N+1){Array(N+1){0}}
-    var jump = Array(N+1){ IntArray(N+1)}
-    var num = 1
-    var x = 1
-    var y = 1
-    jump[y][x++] = num++
-    while(num <= N*N) {
-        if(x == 1 || y == N){
-            while(y >= 1 && x <= N){
-                jump[y--][x++] = num++
-            }
-            if(x > N) {
-                y++
-                x = N
-            }
-            if(y < 1) y = 0
-            y++
-        } else if(y == 1 || x == N){
-            while(x >= 1 && y <= N){
-                jump[y++][x--] = num++
-            }
-            if(y > N) {
-                y = N
-                x++
-            }
-            if(x < 1) x = 0
-            x++
-        }
+    // put maximum number each diagonal line
+    var arr = Array(2*N){0}
+    var max = 0
+    var add = 0
+    for(i in 1 until 2*N){
+        if(i <= N) max += ++add
+        else max += --add
+        arr[i] = max
     }
 
-    x = 1
-    y = 1
-    var ans = 1
     // Rabbit jump
     var string = sc.next()
     val input = string.split("")
+    var ans = 1
+    var x = 1
+    var y = 1
     for(i in input){
         when(i){
             "U" -> {
-                ans += jump[--y][x]
+                ans += findNumber(N, --y, x, arr)
             }
             "D" -> {
-                ans += jump[++y][x]
+                ans += findNumber(N, ++y, x, arr)
             }
             "L" -> {
-                ans += jump[y][--x]
+                ans += findNumber(N, y, --x, arr)
             }
             "R" -> {
-                ans += jump[y][++x]
+                ans += findNumber(N, y, ++x, arr)
             }
         }
     }
     println(ans)
+}
+
+fun findNumber(N : Int, y : Int, x : Int, array : Array<Int>) : Int{
+    val lineNum = y + x - 1
+    var num = array[lineNum]
+
+    if(lineNum <= N){
+        if(lineNum % 2 == 0){
+            var start_y = lineNum
+            var start_x = 1
+
+            while(x != start_x || y != start_y){
+                start_y--
+                start_x++
+                num--
+            }
+        }
+        else {
+            var start_y = 1
+            var start_x = lineNum
+
+            while(x != start_x || y != start_y){
+                start_y++
+                start_x--
+                num--
+            }
+        }
+    }
+    else {
+        if(lineNum % 2 == 0){
+            var start_y = N
+            var start_x = lineNum + 1 - N
+
+            while(x != start_x || y != start_y){
+                start_y--
+                start_x++
+                num--
+            }
+        }
+        else {
+            var start_y = lineNum + 1 - N
+            var start_x = N
+
+            while(x != start_x || y != start_y){
+                start_y++
+                start_x--
+                num--
+            }
+        }
+    }
+    return num
 }
