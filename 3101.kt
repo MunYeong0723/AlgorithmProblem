add9ceb6b0b2135ec0ad40dca0361ee413b9ef40
양문영 <ansdud0723@hanyang.ac.kr>
2020-03-09 23:57:39 +0900

    baekjoon problem #3101 complete

--- 3101.kt
+++ 3101.kt
@@ -7,9 +7,9 @@ fun main() {
     sc.nextLine()
 
     // put maximum number each diagonal line
-    var arr = Array(2*N){0}
-    var max = 0
-    var add = 0
+    var arr = Array(2*N){0L}
+    var max = 0L
+    var add = 0L
     for(i in 1 until 2*N){
         if(i <= N) max += ++add
         else max += --add
@@ -19,9 +19,10 @@ fun main() {
     // Rabbit jump
     var string = sc.next()
     val input = string.split("")
-    var ans = 1
+    var ans = 1L
     var x = 1
     var y = 1
+
     for(i in input){
         when(i){
             "U" -> {
@@ -41,53 +42,17 @@ fun main() {
     println(ans)
 }
 
-fun findNumber(N : Int, y : Int, x : Int, array : Array<Int>) : Int{
+fun findNumber(N : Int, y : Int, x : Int, array : Array<Long>) : Long {
     val lineNum = y + x - 1
     var num = array[lineNum]
 
     if(lineNum <= N){
-        if(lineNum % 2 == 0){
-            var start_y = lineNum
-            var start_x = 1
-
-            while(x != start_x || y != start_y){
-                start_y--
-                start_x++
-                num--
-            }
-        }
-        else {
-            var start_y = 1
-            var start_x = lineNum
-
-            while(x != start_x || y != start_y){
-                start_y++
-                start_x--
-                num--
-            }
-        }
+        if(lineNum % 2 == 0) num -= x - 1
+        else num -= y - 1
     }
     else {
-        if(lineNum % 2 == 0){
-            var start_y = N
-            var start_x = lineNum + 1 - N
-
-            while(x != start_x || y != start_y){
-                start_y--
-                start_x++
-                num--
-            }
-        }
-        else {
-            var start_y = lineNum + 1 - N
-            var start_x = N
-
-            while(x != start_x || y != start_y){
-                start_y++
-                start_x--
-                num--
-            }
-        }
+        if(lineNum % 2 == 0) num -= N - y
+        else num -= N - x
     }
     return num
 }
